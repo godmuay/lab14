4be3a870202beb54fc1faef445ded8f61c6f9618
muay <apinyakasi19@gmail>
2023-01-13 16:19:27 +0700

    14.1-f

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,42 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N, double B[]){
+    double sum =0, sqsum = 0, sqmean ;
+    for (int i=0 ; i<N;i+=1 ){
+        sum+=A[i];
+    }
+    
+     B[0] = sum/N ; //mean
+
+    for(int i=0;i<N;i+=1){
+    sqsum+=pow(A[i],2);
+    }
+    sqmean = pow(sum/N,2);
+    B[1] = sqrt(((sqsum)/N)-sqmean); //SD
+
+    double mul = 1;
+    for(int i=0;i<N;i+=1){
+        mul*=A[i];
+    }
+    B[2] = pow(mul,1.0/N); //GM
+
+    double invsum = 0;
+    for(int i=0;i<N;i+=1){
+        invsum+=(1/A[i]);
+    }
+    B[3] = N/invsum; //HM
+
+    B[4] = A[0];
+    B[5] = A[0];
+    for(int i=0;i<N;i+=1){
+        if(A[i]<B[5])
+        {
+            B[5] = A[i];
+        }//min
+        if(A[i]>B[4]){
+            B[4] = A[i];
+        }//max
+    }
+}
