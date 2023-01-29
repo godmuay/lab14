a6105a8e2ee8520eef32b787c55036a3ec587481
muay <apinyakasi19@gmail>
2023-01-29 22:50:31 +0700

    14.2-f

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -5,19 +5,57 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool [][M],int,int,int);
+void updateImage(bool[][M], int, int, int);
 
-void showImage(const bool [][M]);
+void showImage(const bool[][M]);
 
 int main()
 {
     bool image[N][M] = {};
-    int s,x,y;
-    do{
+    int s, x, y;
+    do
+    {
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
+        updateImage(image, s, x, y);
+    } while (s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void showImage(const bool image[N][M])
+{
+    cout << "------------------------------------------------------------------------\n";
+    for (int i = 0; i < N; i += 1)
+    {
+        cout << "|";
+        for (int j = 0; j < M; j += 1)
+        {
+            if (image[i][j] == 0)
+            {
+                cout << " ";
+            }
+            else if (image[i][j] == 1)
+            {
+                cout << "*";
+            }
+        }
+        cout << "|"
+             << "\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool image[N][M], int s, int x, int y)
+{
+    for (int i = 0; i < N; i += 1)
+    {
+        for (int j = 0; j < M; j += 1)
+        {
+            if (sqrt(pow(i - x, 2) + pow(j - y, 2)) <= s - 1)
+            {
+                image[i][j] = 1;
+            }
+        }
+    }
 }
